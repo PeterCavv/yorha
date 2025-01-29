389cd20ff691121f328026e37169da859c929116
PeterCavv <pedroabad52@gmail.com>
2025-01-29 16:44:23 +0100

    Arreglo fecha guardado Histories.

--- src/main/java/com/dataproject/yorha/entity/History.java
+++ src/main/java/com/dataproject/yorha/entity/History.java
@@ -1,6 +1,8 @@
 package com.dataproject.yorha.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +13,18 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "histories")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id"
+)
 public class History{
 
     @Id
@@ -31,5 +39,5 @@ public class History{
 
     @CreatedDate
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
-    private Date date;
+    private LocalDateTime date;
 }
